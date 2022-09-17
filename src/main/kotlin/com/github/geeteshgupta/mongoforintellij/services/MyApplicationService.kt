7ce479fbf91f09072c859f2646f832e7da11eccf
GitHub Action <action@github.com>
2022-09-17 11:27:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/geeteshgupta/mongoforintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/geeteshgupta/mongoforintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.geeteshgupta.mongoforintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.geeteshgupta.mongoforintellij.MyBundle
 
 class MyApplicationService {
 
